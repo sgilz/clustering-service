573eeffe33e8154b6cb319f4a941c18da07df8fd
fsosap <fsosap@eafit.edu.co>
2021-07-15 16:21:02 -0500

    accounst payable turnover label fixed

--- src/validations.py
+++ src/validations.py
@@ -3,7 +3,7 @@ from cerberus import Validator
 def validate_fields(json: dict):
     schema = {
         "free_cash_flow_to_total_debt": {'required': True, 'type': 'float'},  
-        "accounts_ayable_turnover":{'required': True, 'type': 'float'}, 
+        "accounts_payable_turnover":{'required': True, 'type': 'float'}, 
         "operating_margin": {'required': True, 'type': 'float'}, 
         "sales_per_employee": {'required': True, 'type': 'float'}, 
         "asset_turnover": {'required': True, 'type': 'float'}, 
